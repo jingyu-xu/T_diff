8a297cc29c910ee0ebd0ac9ab80dc72cceaaa2ac
jingyu-xu <phoebexu.umass@gmail.com>
2016-06-07 01:03:05 -0400

    6/6/2016 solve some bug 1. change the tumor radius from 10 mm to 7 mm 2. add some code for plot Temperature difference

--- T_temp_diff.m
+++ T_temp_diff.m
@@ -80,9 +80,9 @@ hcb = colorbar;
 set(hcb,'YTick',[0,1,2,3,4,5,6,7,8],'YTickLabel',{'Air','Skin','Gland-1','Gland-2','Gland-3','Fat-1','Fat-2','Fat-3','Muscle'},'FontSize',14);
 
 % create temperature model
-tumor_on = 0; tumor_depth = 8;  tumor_radius = 10; Tambient = 27; Tart = 37; 
+tumor_on = 0; tumor_depth = 10;  tumor_radius = 7; Tambient = 27; Tart = 37; tum_y_cen = 90; tum_z_cen = floor(s3_ss/2);tum_x_cen = 20 + tumor_depth;
 % muscle_wall = 154; skin_start = 0; % can't remember why I put this in
-[T_3d_nom,tissue_3d_nom] = gen_breast_therm_model(model,s1_ss,s2_ss,s3_ss,tumor_on,tumor_depth,tumor_radius,Tambient,Tart,muscle_wall,skin_start);
+[T_3d_nom,tissue_3d_nom] = gen_breast_therm_model(model,s1_ss,s2_ss,s3_ss,tumor_on,tumor_depth,tumor_radius,Tambient,Tart,muscle_wall,skin_start,tum_x_cen,tum_y_cen,tum_z_cen);
 figure; contourf(T_3d_nom(:,:,floor(s3_ss/2)));
 colorbar;
 xlabel('Distance (mm)','FontSize',14); ylabel('Distance (mm)','FontSize',14);
@@ -90,13 +90,67 @@ title('Normal Temperature Profile (\circC)','FontSize',14);
 
 % Generate temperature anomalies with radius = 10
 tumor_on = 1; tum_y_cen = 90; tum_z_cen = floor(s3_ss/2);
-
-tumor_radius = 10; tumor_depth = 10; tum_x_cen = 20 + tumor_depth;% tumor dept is 1cm
+tumor_radius = 7; tumor_depth = 10; tum_x_cen = 20 + tumor_depth;
 [T_3d_abn1,tissue_3d_abn1] = gen_breast_therm_model(model,s1_ss,s2_ss,s3_ss,tumor_on,tumor_depth,tumor_radius,Tambient,Tart,muscle_wall,skin_start,tum_x_cen,tum_y_cen,tum_z_cen);
+tumor_radius = 7; tumor_depth = 40; tum_x_cen = 20 + tumor_depth; tumor_on = 1; tum_y_cen = 50; tum_z_cen = floor(s3_ss/2);
+[T_3d_abn2,tissue_3d_abn2] = gen_breast_therm_model(model,s1_ss,s2_ss,s3_ss,tumor_on,tumor_depth,tumor_radius,Tambient,Tart,muscle_wall,skin_start,tum_x_cen,tum_y_cen,tum_z_cen);
+tumor_radius = 7; tumor_depth = 40; tum_x_cen = 20 + tumor_depth; tumor_on = 1; tum_y_cen = 130; tum_z_cen = floor(s3_ss/2);
+[T_3d_abn3,tissue_3d_abn3] = gen_breast_therm_model(model,s1_ss,s2_ss,s3_ss,tumor_on,tumor_depth,tumor_radius,Tambient,Tart,muscle_wall,skin_start,tum_x_cen,tum_y_cen,tum_z_cen);
+tumor_radius = 7; tumor_depth = 40; tum_x_cen = 20 + tumor_depth; tumor_on = 1; tum_y_cen = 90; tum_z_cen = floor(s3_ss/2);
+[T_3d_abn4,tissue_3d_abn4] = gen_breast_therm_model(model,s1_ss,s2_ss,s3_ss,tumor_on,tumor_depth,tumor_radius,Tambient,Tart,muscle_wall,skin_start,tum_x_cen,tum_y_cen,tum_z_cen);
+tumor_radius = 7; tumor_depth = 70; tum_x_cen = 20 + tumor_depth; tumor_on = 1; tum_y_cen = 90; tum_z_cen = floor(s3_ss/2);
+[T_3d_abn5,tissue_3d_abn5] = gen_breast_therm_model(model,s1_ss,s2_ss,s3_ss,tumor_on,tumor_depth,tumor_radius,Tambient,Tart,muscle_wall,skin_start,tum_x_cen,tum_y_cen,tum_z_cen);
 
-T_3d_diff = T_3d_abn1 - T_3d_nom;
+
+figure; contourf(T_3d_abn1(:,:,floor(s3_ss/2)));
+xlabel('Distance (mm)','FontSize',14); ylabel('Distance (mm)','FontSize',14);
+hcb = colorbar; 
+set(hcb,'YTick',[0,1,2,3,4,5,6,7,8],'YTickLabel',{'Air','Skin','Gland-1','Gland-2','Gland-3','Fat-1','Fat-2','Fat-3','Muscle'},'FontSize',14);
+title('Temperature Profile of 1cm Deep Tumor (\circC)','FontSize',14);
+
+figure; contourf(T_3d_abn2(:,:,floor(s3_ss/2)));
+xlabel('Distance (mm)','FontSize',14); ylabel('Distance (mm)','FontSize',14);
+hcb = colorbar; 
+set(hcb,'YTick',[0,1,2,3,4,5,6,7,8],'YTickLabel',{'Air','Skin','Gland-1','Gland-2','Gland-3','Fat-1','Fat-2','Fat-3','Muscle'},'FontSize',14);
+title('Temperature Profile of 3cm Deep left Tumor (\circC)','FontSize',14);
+
+figure; contourf(T_3d_abn3(:,:,floor(s3_ss/2)));
+xlabel('Distance (mm)','FontSize',14); ylabel('Distance (mm)','FontSize',14);
+hcb = colorbar; 
+set(hcb,'YTick',[0,1,2,3,4,5,6,7,8],'YTickLabel',{'Air','Skin','Gland-1','Gland-2','Gland-3','Fat-1','Fat-2','Fat-3','Muscle'},'FontSize',14);
+title('Temperature Profile of 3cm Deep right Tumor (\circC)','FontSize',14);
+
+figure; contourf(T_3d_abn4(:,:,floor(s3_ss/2)));
+xlabel('Distance (mm)','FontSize',14); ylabel('Distance (mm)','FontSize',14);
+hcb = colorbar; 
+set(hcb,'YTick',[0,1,2,3,4,5,6,7,8],'YTickLabel',{'Air','Skin','Gland-1','Gland-2','Gland-3','Fat-1','Fat-2','Fat-3','Muscle'},'FontSize',14);
+title('Temperature Profile of center Tumor (\circC)','FontSize',14);
+
+figure; contourf(T_3d_abn5(:,:,floor(s3_ss/2)));
+xlabel('Distance (mm)','FontSize',14); ylabel('Distance (mm)','FontSize',14);
+hcb = colorbar; 
+set(hcb,'YTick',[0,1,2,3,4,5,6,7,8],'YTickLabel',{'Air','Skin','Gland-1','Gland-2','Gland-3','Fat-1','Fat-2','Fat-3','Muscle'},'FontSize',14);
+title('Temperature Profile of 5cm Deep Tumor (\circC)','FontSize',14);
+
+T_3d_diff1 = T_3d_abn1 - T_3d_nom;
+T_3d_diff2 = T_3d_abn2 - T_3d_nom;
+T_3d_diff3 = T_3d_abn3 - T_3d_nom;
+T_3d_diff4 = T_3d_abn4 - T_3d_nom;
+T_3d_diff5 = T_3d_abn5 - T_3d_nom;
+
 figure
-plot(T_3d_diff(20:100,90,80),'k','LineWidth',1);
+hold on;
+plot(T_3d_diff1(20:100,90,77),'k-','LineWidth',1);
+plot(T_3d_diff2(20:100,50,77),'r--','LineWidth',1);
+plot(T_3d_diff3(20:100,130,77),'m:','LineWidth',1);
+plot(T_3d_diff4(20:100,90,77),'y-.','LineWidth',1);
+plot(T_3d_diff5(20:100,90,77),'b','LineWidth',1);
+title('Temperature Difference due to the tumor')
+hleg1 = legend('front','left','right','center','bottom');
+set(hleg1,'Location','Northeast')
+hold off
+
+
 
 
 
